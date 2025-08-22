b91cfad86832b4ca756906c73464183b6bcdedde
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-22 11:09:27 +0200

    added Bonus Tasks

--- bin/aufgabe3451.dart
+++ bin/aufgabe3451.dart
@@ -12,11 +12,13 @@ List<String> names =
     'Leonhart'
   ];
 
+  // Aufgabe 1
   for(String n in names)
   {
     print(n);
   }
 
+  // Aufgabe 2
   double pointsAverage = 0.0;
   for(int p in points)
   {
@@ -24,4 +26,28 @@ List<String> names =
   }
   pointsAverage /= points.length;
   print("Durchschnittliche Punktezahl: $pointsAverage");
+
+
+  // Bonus 1
+  Map<String, int>? scoreMap = {};
+
+  for (int i = 0; i < names.length; i++) {
+    scoreMap[names[i]] = points[i];
+  }
+
+  // Bonus 2
+  for( var n in scoreMap.entries)
+  {
+    print(n.key);
+  }
+
+  for( var p in scoreMap.entries)
+  {
+    print(p.value);
+  }
+
+  for (var entry in scoreMap.entries) 
+  {
+    print('${entry.key}: ${entry.value}');
+  }
 }
