178a1ac036e67a7bd20f6992dcd8dd69c82e5235
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-22 10:45:39 +0200

    init

---
+++ bin/aufgabe3451.dart
@@ -0,0 +1,18 @@
+void main(List<String> arguments) {
+
+List<String> names = 
+  [
+    'Julietta',
+    'Benjamino',
+    'Hans-Günther',
+    'Evalinea',
+    'Fiona',
+    'Gregory',
+    'Leonhart'
+  ];
+
+  for(String n in names)
+  {
+    print(n);
+  }
+}
